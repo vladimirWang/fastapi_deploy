f3b8baef84403897c20d8e20f4b1efd11995854c
fernando <413114463@qq.com>
2026-05-28 09:52:36 +0800

    测试安装依赖

--- user_create.sh
+++ user_create.sh
@@ -1,5 +1,5 @@
 resp=$(curl -sS -X POST "http://127.0.0.1:6000/user" \
   -H "Content-Type: application/json" \
-  -d '{"username":"good"}' | jq .)
+  -d '{"username":"monday"}' | jq .)
 
 echo "$resp"
